d37c512bc9daae257c39b0aa1ddc935d7c193ae5
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-14 08:58:29 -0800

    completed cards component, completed mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,38 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const parent = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        console.log(res.data);
+        Object.values(res.data.articles).forEach(item => {
+            item.forEach(item => parent.append(articleMaker(item)))
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
+function articleMaker(obj) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = obj.headline;
+    let author = document.createElement('div');
+    author.classList.add('author');
+    let imageContainer = document.createElement('div');
+    imageContainer.classList.add('img-container');
+    let image = document.createElement('img');
+    image.src = obj.authorPhoto;
+    let name = document.createElement('span');
+    name.textContent = obj.authorName;
+    
+    imageContainer.append(image);
+    author.append(imageContainer, name);
+    card.append(headline, author);
+
+    return card;
+}
